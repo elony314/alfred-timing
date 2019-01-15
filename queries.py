28344accb6253d7a741fb89b0df07411bd74f275
Elon Yang <elony314@gmail.com>
2019-01-15 02:43:52 -0800

    rewrite the query part

--- queries.py
+++ queries.py
@@ -8,155 +8,32 @@ Defines queries and execution functions for calling the Bear sqlite DB.
 import sqlite3
 import os
 
-DB_LOCATION_OLD_OLD = (
-    "/Library/Containers/net.shinyfrog.bear/Data/Library/Application Support/"
-    "net.shinyfrog.bear/database.sqlite")
-DB_LOCATION_OLD = (
-    "/Library/Containers/net.shinyfrog.bear/Data/Documents/Application Data/"
-    "database.sqlite")
-DB_LOCATION = (
-    "/Library/Group Containers/9K33E3U3T4.net.shinyfrog.bear/Application Data/"
-    "database.sqlite")
-DB_KEY = 'db_path'
+DB_LOCATION = "⁨/⁨Library⁩/⁨Application Support⁩/⁨info.eurocomp.Timing2⁩/SQLite.db"
+DB_KEY = "db_path"
 
-NOTES_BY_TITLE = (
-    "SELECT DISTINCT"
-    "   ZUNIQUEIDENTIFIER, ZTITLE "
-    "FROM "
-    "   ZSFNOTE "
-    "WHERE "
-    "   ZARCHIVED=0 "
-    "   AND ZTRASHED=0 "
-    "   AND lower(ZTITLE) LIKE lower('%{0}%')"
-    "ORDER BY "
-    "   ZMODIFICATIONDATE DESC")
+RECENT_TASKS =  "SELECT DISTINCT TaskActivity.title, Project.title FROM TaskActivity INNER JOIN Project ON TaskActivity.projectID=Project.id WHERE TaskActivity.isDeleted=0 ORDER BY TaskActivity.endDate DESC LIMIT 10"
+  
+TASKS_BY_TITLE = "SELECT DISTINCT TaskActivity.title, Project.title FROM TaskActivity INNER JOIN Project ON TaskActivity.projectID=Project.id WHERE TaskActivity.isDeleted=0 AND lower(TaskActivity.title) LIKE lower('%{0}%')"
 
-NOTES_BY_TEXT = (
-    "SELECT DISTINCT"
-    "   ZUNIQUEIDENTIFIER, ZTITLE "
-    "FROM "
-    "   ZSFNOTE "
-    "WHERE "
-    "   ZARCHIVED=0 "
-    "   AND ZTRASHED=0 "
-    "   AND lower(ZTEXT) LIKE lower('%{0}%')"
-    "ORDER BY "
-    "   ZMODIFICATIONDATE DESC"
-)
-
-TAGS_BY_TITLE = (
-    "SELECT DISTINCT"
-    "   t.ZTITLE "
-    "FROM "
-    "   ZSFNOTE n "
-    "   INNER JOIN Z_5TAGS nt ON n.Z_PK = nt.Z_5NOTES "
-    "   INNER JOIN ZSFNOTETAG t ON nt.Z_10TAGS = t.Z_PK "
-    "WHERE "
-    "   n.ZARCHIVED=0 "
-    "   AND n.ZTRASHED=0 "
-    "   AND lower(t.ZTITLE) LIKE lower('%{0}%')"
-    "ORDER BY "
-    "   t.ZMODIFICATIONDATE DESC")
-
-TAGS_BY_TITLE_V6 = (
-    "SELECT DISTINCT"
-    "   t.ZTITLE "
-    "FROM "
-    "   ZSFNOTE n "
-    "   INNER JOIN Z_6TAGS nt ON n.Z_PK = nt.Z_6NOTES "
-    "   INNER JOIN ZSFNOTETAG t ON nt.Z_13TAGS = t.Z_PK "
-    "WHERE "
-    "   n.ZARCHIVED=0 "
-    "   AND n.ZTRASHED=0 "
-    "   AND lower(t.ZTITLE) LIKE lower('%{0}%')"
-    "ORDER BY "
-    "   t.ZMODIFICATIONDATE DESC")
-
-NOTES_BY_TAG_TITLE = (
-    "SELECT DISTINCT"
-    "   n.ZUNIQUEIDENTIFIER, n.ZTITLE "
-    "FROM "
-    "   ZSFNOTE n "
-    "   INNER JOIN Z_5TAGS nt ON n.Z_PK = nt.Z_5NOTES "
-    "   INNER JOIN ZSFNOTETAG t ON nt.Z_10TAGS = t.Z_PK "
-    "WHERE "
-    "   n.ZARCHIVED=0 "
-    "   AND n.ZTRASHED=0 "
-    "   AND lower(t.ZTITLE) LIKE lower('%{0}%')"
-    "ORDER BY "
-    "   n.ZMODIFICATIONDATE DESC")
-
-NOTES_BY_TAG_TITLE_V6 = (
-    "SELECT DISTINCT"
-    "   n.ZUNIQUEIDENTIFIER, n.ZTITLE "
-    "FROM "
-    "   ZSFNOTE n "
-    "   INNER JOIN Z_6TAGS nt ON n.Z_PK = nt.Z_6NOTES "
-    "   INNER JOIN ZSFNOTETAG t ON nt.Z_13TAGS = t.Z_PK "
-    "WHERE "
-    "   n.ZARCHIVED=0 "
-    "   AND n.ZTRASHED=0 "
-    "   AND lower(t.ZTITLE) LIKE lower('%{0}%')"
-    "ORDER BY "
-    "   n.ZMODIFICATIONDATE DESC")
-
-
-def search_notes_by_title(workflow, log, query):
+def list_recent_tasks(workflow, log, query):
     """
-    Searches for Bear notes by the title of the note.
+    List recent tasks
     """
-
-    sql_query = NOTES_BY_TITLE.format(query)
+    sql_query = RECENT_TASKS
     return run_query(workflow, log, sql_query)
-
-
-def search_notes_by_text(workflow, log, query):
+    
+def search_tasks_by_title(workflow, log, query):
     """
-    Searches for Bear notes by the content of the note.
+    Searches for tasks by the title of the task.
     """
-
-    sql_query = NOTES_BY_TEXT.format(query)
+    sql_query = TASKS_BY_TITLE.format(query)
     return run_query(workflow, log, sql_query)
-
-
-def search_tags_by_title(workflow, log, query):
-    """
-    Searches for Bear tags by tag name.
-    """
-
-    sql_query = TAGS_BY_TITLE_V6.format(query)
-
-    home = os.path.expanduser("~")
-    if find_bear_db(log) == "{0}{1}".format(home, DB_LOCATION_OLD):
-        sql_query = TAGS_BY_TITLE.format(query)
-
-    return run_query(workflow, log, sql_query)
-
-
-def search_notes_by_tag_title(workflow, log, query):
-    """
-    Searches for Bear notes by tag name.
-    """
-
-    sql_query = NOTES_BY_TAG_TITLE_V6.format(query)
-
-    home = os.path.expanduser("~")
-    if find_bear_db(log) == "{0}{1}".format(home, DB_LOCATION_OLD):
-        sql_query = NOTES_BY_TAG_TITLE.format(query)
-
-    return run_query(workflow, log, sql_query)
-
-
+    
 def run_query(workflow, log, sql):
     """
     Takes a SQL command, executes it, and returns the results.
     """
-
     db_path = workflow.stored_data(DB_KEY)
-    home = os.path.expanduser("~")
-    if db_path and db_path == "{0}{1}".format(home, DB_LOCATION_OLD):
-        db_path = find_bear_db(log)
-        workflow.store_data(DB_KEY, db_path)
     if not db_path:
         db_path = find_bear_db(log)
         workflow.store_data(DB_KEY, db_path)
@@ -172,26 +49,16 @@ def run_query(workflow, log, sql):
     cursor.close()
     return results
 
-
-def find_bear_db(log):
+def find_timingdb(log):
     """
-    Finds the Bear sqlite3 DB.
+    Finds the Timing sqlite3 DB.
     """
-
     home = os.path.expanduser("~")
     db_file = "{0}{1}".format(home, DB_LOCATION)
 
     if not os.path.isfile(db_file):
         log.debug(
-            "Bear db not found at {0}".format(db_file))
-        db_file = "{0}{1}".format(home, DB_LOCATION_OLD)
-        if not os.path.isfile(db_file):
-            log.debug(
-                "Bear db not found at {0}".format(db_file))
-            db_file = "{0}{1}".format(home, DB_LOCATION_OLD_OLD)
-            if not os.path.isfile(db_file):
-                log.debug(
-                    "Bear db not found at {0}".format(db_file))
+            "Timing db not found at {0}".format(db_file))
 
     log.debug(db_file)
     return db_file
